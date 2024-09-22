441ea5a87020a256d8330170bf57e8e6f85112ac
lucas <andrebastoslucas@gmail.com>
2024-09-22 17:18:27 -0300

    tratando possiveis valores nulls

--- app/views/view.ts
+++ app/views/view.ts
@@ -3,7 +3,12 @@ export abstract class View<T> {
   private escapar: boolean = false;
 
   constructor(seletor: string, escapar?: boolean) {
-    this.elemento = document.querySelector(seletor);
+    const elemento = document.querySelector(seletor);
+    if (elemento) {
+      this.elemento = elemento as HTMLInputElement;
+    } else {
+      throw Error(`Seletor ${seletor} não foi encontrado, verifique.`);
+    }
     if (escapar) {
       this.escapar = escapar;
     }
